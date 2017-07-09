422b4fdeb2ea7457ba9d806cbbd59d02050cf057
Kurt Zikaras <KurtZikaras@Kurts-MacBook-Pro.local>
2017-07-08 23:38:00 -0400

    begin adding event logs

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,15 +1,23 @@
 alert("Why hello there! I am a wee bit of JavaScript.");
-			var animatePoints = function(){
-				var points = document.getElementsByClassName('point');
-				var revealPoint = function(index){
-					index.style.opacity = 1;
-					index.style.transform = "scaleX(1) translateY(0)";
-					index.style.msTransform = "scaleX(1) translateY(0)";
-					index.style.WebkitTransform = "scaleX(1) translateY(0)";
-					}
-				for(i=0;i<points.length;i++){
-					revealPoint(points[i]);
-				}
-			};
-			animatePoints();
+var pointsArray = document.getElementsByClassName('point');
+var animatePoints = function(points) {
+	var revealPoint = function(index) {
+		points[index].style.opacity = 1;
+		points[index].style.transform = "scaleX(1) translateY(0)";
+		points[index].style.msTransform = "scaleX(1) translateY(0)";
+		points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+		
+	}
+	for(i=0;i<points.length;i++){
+		revealPoint(i);
+}
+			
+
+window.onload = function() {
+	var sellingPoints = document.getElementsByClassName('selling-points')[0];
+	window.addEventListener('scroll', function(event) {
+		console.log("Current offset from the top is " + sellingPoints.getBoundingClientRect().top + " pixels");
+	});
+}
+}			
 			
