eb01e0e13bd584fbb9d0f7368204a07ce78070d8
Kurt Zikaras <KurtZikaras@Kurts-MacBook-Pro.local>
2017-07-09 09:03:55 -0400

    Add event handlers

--- scripts/landing.js
+++ scripts/landing.js
@@ -10,14 +10,21 @@ var animatePoints = function(points) {
 	}
 	for(i=0;i<points.length;i++){
 		revealPoint(i);
+		console.log('point');
+	}
 }
 			
 
 window.onload = function() {
+	if(window.innerHeight > 950){
+		animatePoints(pointsArray);
+	}
 	var sellingPoints = document.getElementsByClassName('selling-points')[0];
+	var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
 	window.addEventListener('scroll', function(event) {
-		console.log("Current offset from the top is " + sellingPoints.getBoundingClientRect().top + " pixels");
+		if(document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+			animatePoints(pointsArray);
+		}
 	});
-}
 }			
 			
