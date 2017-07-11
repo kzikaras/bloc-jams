301c3b08ad2587b5f5163324ed76b4191a49332c
Kurt Zikaras <KurtZikaras@Kurts-MacBook-Pro.local>
2017-07-10 21:27:34 -0400

    Add play button to album.js

--- scripts/album.js
+++ scripts/album.js
@@ -55,7 +55,7 @@ var vanHalen = {
 var createSongRow = function(songNumber, songName, songLength){
 	var template = 
 		'<tr class="album-view-song-item">'
-		+'<td class="song-item-number">' + songNumber + '</td>'
+		+'<td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
 		+'<td class="song-item-title">' + songName + '</td>'
 		+'<td class="song-item-duration">' + songLength + '</td>'
 		+'</tr>'
@@ -103,11 +103,59 @@ var changeAlbum = function(album) {
 	return album;
 };
 
+var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+var songRows = document.getElementsByClassName('album-view-song-item');
+
+var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+
 window.onload = function(){
 	setCurrentAlbum(vanHalen);
 	var currentAlbum = 'van halen';
 	document.getElementsByClassName('album-cover-art')[0].addEventListener('click', function() {
 		currentAlbum = changeAlbum(currentAlbum);
 	});	
+	songListContainer.addEventListener('mouseover', function(event) {
+		// Only target individual song rows during event delegation
+		if(event.target.parentElement.className === 'album-view-song-item'){
+			// change the content from the number to the play button's html
+			event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+		}
+		
+	});
+
+	for(var i=0;i<songRows.length;i++){
+		songRows[i].addEventListener('mouseleave', function(event) {
+			// revert the content back to the number
+			// selects first child element, which is the song-item-number element
+			this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+		});
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 		
