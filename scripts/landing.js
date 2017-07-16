1ac13a9c899cbfedbf089f634d3739dc91be4928
Kurt Zikaras <KurtZikaras@Kurts-MacBook-Pro.local>
2017-07-16 16:48:49 -0400

    Sunday work

--- scripts/landing.js
+++ scripts/landing.js
@@ -13,17 +13,20 @@ alert("Why hello there! I am a wee bit of JavaScript.");
 			animatePoints();
 			
 
-window.onload = function() {
-	if(window.innerHeight > 950){
-		animatePoints(pointsArray);
+$(window).load(function() {
+	// #1
+	if ($(window).height() > 950){
+		animatePoints();		
 	}
-	var sellingPoints = document.getElementsByClassName('selling-points')[0];
-	var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
-	window.addEventListener('scroll', function(event) {
-		if(document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-			animatePoints(pointsArray);
-		}
+	// #2
+	var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
+	// #3
+	$(window).scroll(function(event) {
+		// #4
+		if ($(window).scrollTop() >= scrollDistance) {
+			animatePoints();
+		}	
 	});
-}			
+});		
 
 			
