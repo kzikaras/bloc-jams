b1cca383a7cdac92f558e5d87593f81d66f7087b
Kurt Zikaras <KurtZikaras@Kurts-MacBook-Pro.local>
2017-07-23 19:19:19 -0400

    save

--- scripts/album.js
+++ scripts/album.js
@@ -106,6 +106,30 @@ var setCurrentAlbum = function(album) {
 	}
 };
 
+var updateSeekPercentage = function($seekBar, seekBarFillRatio) {
+	var offsetXPercent = seekBarFillRatio * 100;
+	// #1
+	offsetXPercent = Math.max(0, offsetXPercent);
+	offsetXPercent = Math.min(100, offsetXPercent);
+	// #2
+	var percentageString = offsetXPercent + '%';
+	$seekBar.find('.fill').width(percentageString);
+	$seekBar.find('.thumb').css({left: percentageString});
+};
+
+var setupSeekBars = function() {
+	var $seekBars.click(function(event) {
+		// #3
+		var offsetX = event.pageX - $(this).offset().left;
+		var barWidth = $(this).width();
+		// #4
+		var seekBarFillRatio = offsetX/barWidth;
+
+		// #5
+		updateSeekPercentage($(this), seekBarFillRatio);
+	});
+};
+
 var trackIndex = function(album, song) {
 	return album.songs.indexOf(song);
 }
